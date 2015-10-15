da1974d544cb076d9b61a26a9a51aa143335f25b
Ryan Tate <ryan.michael.tate@gmail.com>
2015-10-15 11:13:06 -0700

    connect to VEX private network

--- test.js
+++ test.js
@@ -4,30 +4,14 @@ var Vex = require('./index.js');
 var net = require('net');
 var path = require('path');
 
-console.log(Vex.getPrivateNetworkArguments());
+Vex.joinNetwork();
 
-Vex.Personal.newAccount('white', 'rabbit', function(address, password){
-	Vex.Personal.unlockAccount(address, password, 200, function(data){
-		console.log(data);
-	});
-});
 
-Vex.TxPool.status(function(pending, queued){
-	console.log(pending);
-	console.log(queued);
-});
+setTimeout(function(){
+	Vex.Web3().version.getNetwork(function(err, result){
+		console.log(err);
+		console.log(result);
+	})
+}, 12000);
 
-Vex.Admin.chainSyncStatus(function(blocksAvailable, blocksWaitingForImport, estimate, importing){
-	console.log(blocksAvailable)
-	console.log(blocksWaitingForImport)
-	console.log(estimate)
-	console.log(importing)
-});
 
-Vex.Admin.verbosity(4, function(data){
-	console.log(data);
-});
-
-Vex.Admin.nodeInfo(function(data){
-	console.log(data);
-});
